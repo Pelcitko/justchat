325b96b146b6b1a20c0543ded3d95f8b3b5efe53
Aasz <pelc.it@gmail.com>
2020-02-18 23:43:46 +0100

    Nastavení STATICFILES_STORAGE pro vyšší verzi Whitenoise

--- justchat/settings.py
+++ justchat/settings.py
@@ -111,7 +111,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# old: STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # authentication settings
 
