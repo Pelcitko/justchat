a1af7f1bdb4dcd42738de8f49f96d60b7bfa7abc
Aasz <pelc.it@gmail.com>
2020-02-19 17:17:51 +0100

    + další porty

--- justchat/settings.py
+++ justchat/settings.py
@@ -138,9 +138,13 @@ if DEBUG:
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000',
+    'localhost:8000',
+    'localhost:8080',
     'http://localhost:3000',
     'https://localhost:3000',
     '127.0.0.1:3000',
+    '127.0.0.1:8000',
+    '127.0.0.1:8090',
     'http://127.0.0.1:3000',
     'https://127.0.0.1:3000',
     )
